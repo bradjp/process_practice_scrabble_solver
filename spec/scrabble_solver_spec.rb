6ddd5e2566553814a6850e100f2a02e9d48f4a0b
Brad Phillips <bjrp@protonmail.com>
2019-12-24 16:24:55 +0000

    Passes tests for escaped characters and nil

--- spec/scrabble_solver_spec.rb
+++ spec/scrabble_solver_spec.rb
@@ -6,7 +6,15 @@ describe 'Scrabble' do
     expect(scrabble.word).to eq('hello')
   end
   it 'returns a score of 0 when passed an empty string' do
-  scrabble = Scrabble.new('')
-  expect(scrabble.score).to eq(0)
+    scrabble = Scrabble.new('')
+    expect(scrabble.score).to eq(0)
+  end
+  it 'returns a score of 0 when passed escaped characters' do
+    scrabble = Scrabble.new(" \t\n")
+    expect(scrabble.score).to eq(0)
+  end
+  it 'returns a score of 0 when passed nil' do
+    scrabble = Scrabble.new(nil)
+    expect(scrabble.score).to eq(0)
   end
 end
