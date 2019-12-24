0d65d3dc362a32569b382fbba91f6df8eb7d60c8
Brad Phillips <bjrp@protonmail.com>
2019-12-24 20:15:51 +0000

    Extract letters hash to constant

--- lib/scrabble_solver.rb
+++ lib/scrabble_solver.rb
@@ -1,4 +1,9 @@
 class Scrabble
+
+  LETTER_SCORES = {'a' => 1, 'b' => 3, 'c' => 3, 'd' => '2', 'e' => 1, 'f' => 4, 'g' => 2, 'h' => 4, 'i' => 1, 'j' => 8, 'k' => 5,
+  'l' => 1, 'm' => 3, 'n' => 1, 'o' => 1, 'p' => 3, 'q' => 10, 'r' => 1, 's' => 1, 't' => 1, 'u' => 1, 'v' => 4, 'w' => 4, 'x' => 8,
+  'y' => 4, 'z' => 10}
+
   def initialize(word)
     @word = word
   end
@@ -12,11 +17,8 @@ class Scrabble
     return 0 if @word.match(/[a-zA-Z]/) == nil
 
     letters = @word.downcase.split('')
-    letter_scores = {'a' => 1, 'b' => 3, 'c' => 3, 'd' => '2', 'e' => 1, 'f' => 4, 'g' => 2, 'h' => 4, 'i' => 1, 'j' => 8, 'k' => 5,
-    'l' => 1, 'm' => 3, 'n' => 1, 'o' => 1, 'p' => 3, 'q' => 10, 'r' => 1, 's' => 1, 't' => 1, 'u' => 1, 'v' => 4, 'w' => 4, 'x' => 8,
-    'y' => 4, 'z' => 10}
 
-    letters.map { |letter| letter_scores[letter] }.inject(:+)
+    letters.map { |letter| LETTER_SCORES[letter] }.inject(:+)
    
   end
   
